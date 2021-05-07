7c65a8ae0542a79f1c885b4a094deb3d4e2c0596
jiweiqi <jiweiqi10@gmail.com>
2021-05-07 00:22:06 -0400

    still not working

--- src/flame_1d.jl
+++ src/flame_1d.jl
@@ -19,28 +19,7 @@ function (cal_wdot::Wdot)(u, p)
     kp = exp.(p)
     qdot = wdot_func(gas.reaction, T, C, S0, h_mole; get_qdot=true) .* kp
     wdot = gas.reaction.vk * qdot
-    # Ydot = wdot / ρ_mass .* gas.MW
-    # Tdot = -dot(h_mole, wdot) / ρ_mass / cp_mass
-    Ydot = wdot
-    Tdot = - sum(h_mole .* wdot, dims=1)
-    du = vcat(Ydot, Tdot)
-    return du
-end
-
-function get_cpk(gas, T, X, mean_MW)
-    cp_T = [1.0, T, T^2, T^3, T^4]
-    if T <= 1000.0
-        cp = @view(gas.thermo.nasa_low[:, 1:5]) * cp_T
-    else
-        cp = @view(gas.thermo.nasa_high[:, 1:5]) * cp_T
-    end
-    # TODO: not sure if inplace operation will be an issue for AD
-    if !gas.thermo.isTcommon
-        ind_correction = @. (T > 1000.0) & (T < gas.thermo.Trange[:, 2])
-        cp[ind_correction] .=
-            @view(gas.thermo.nasa_low[ind_correction, 1:5]) * cp_T
-    end
-    return cp
+    return wdot
 end
 
 function dydz!(dy, z, y)
@@ -53,34 +32,60 @@ function dydzm!(dy, z, y)
     @. dy[:, 1] = (y[:, 2] - y[:, 1]) / (z[2] - z[1])
 end
 
+function dydz_central!(dy, z, y)
+
+    hj = z[3:end] - z[2:end - 1]
+    hjL = z[2:end - 1] - z[1:end - 2]
+    c1 = @. hjL / hj / (hj + hjL)
+    c2 = @. (hj - hjL) / hj / hjL
+    c3 = @. hj / hjL / (hj + hjL)
+    @. dy[2:end - 1] = c1 * y[3:end] + c2 * y[2:end - 1] - c3 * y[1:end - 2]
+
+    dy[1] = (y[2] - y[1]) / (z[2] - z[1])
+    dy[end] = (y[end] - y[end - 1]) / (z[end] - z[end - 1])
+end
+
+function dydzm_central!(dy, z, y)
+
+    hj = z[3:end] - z[2:end - 1]
+    hjL = z[2:end - 1] - z[1:end - 2]
+    c1 = @. hjL / hj / (hj + hjL)
+    c2 = @. (hj - hjL) / hj / hjL
+    c3 = @. hj / hjL / (hj + hjL)
+
+    @. dy[:, 2:end - 1] = c1' * y[:, 3:end] + c2' * y[:, 2:end - 1] - c3' * y[:, 1:end - 2]
+    
+    @. dy[:, 1] = (y[:, 2] - y[:, 1]) / (z[2] - z[1])
+    @. dy[:, end] = (y[:, end] - y[:, end - 1]) / (z[end] - z[end - 1])
+end
+
 
 """
 p: params
 grid: grid points
-y: solution variables: [Y, T, u] {ns+2, ng}
 yL: left boundary conditions
 Equation @ https://cantera.org/science/flames.html
 """
 function residual(gas, cal_wdot, p, z, yv, yL, ind_f, T_f)
 
     ng = length(z)
-    y = reshape(yv, ng, ny)'
+    y = reshape(@view(yv[1:end - 1]), ny, ng)
+    mdot = yv[end]
 
     mY = @view(y[1:ns, :])
     mT = @view(y[ns + 1, :])
-    mu = @view(y[ns + 2, :])
 
     m_η_mix = similar(mT)
     m_λ_mix = similar(mT)
     m_Dkm = similar(mY)
     m_X = similar(mY)
+    m_u = similar(mT)
     m_ρ = similar(mT)
     m_mean_MW = similar(mT)
-    m_cp_mole = similar(mT)
     m_cp_mass = similar(mT)
     m_cpk = similar(mY)
     m_h_mole = similar(mY)
-    m_wdot = similar(y[1:ns + 1, :]) * p[1]
+    m_wdot = similar(mY) * p[1]
 
     for i = 1:ng
         T = mT[i]
@@ -96,95 +101,73 @@ function residual(gas, cal_wdot, p, z, yv, yL, ind_f, T_f)
         m_X[:, i] = X
         m_ρ[i] = ρ_mass
         m_mean_MW[i] = mean_MW
+        m_u[i] = mdot / ρ_mass
 
         cp_mole, cp_mass = get_cp(gas, T, X, mean_MW)
-        m_cp_mole[i] = cp_mole
         m_cp_mass[i] = cp_mass
-        m_cpk[:, i] = get_cpk(gas, T, X, mean_MW)
+        m_cpk[:, i] = cal_cpmass(gas, T, 0.0, X)
         m_h_mole[:, i] = get_H(gas, T, Y, X)
 
         m_wdot[:, i] = cal_wdot(@view(y[1:ns + 1, i]), p)
     end
 
+    # update_dYdx
     dXdz = similar(m_X)
-    dydzm!(dXdz, z, m_X)
+    dydzm_central!(dXdz, z, m_X)
+    
     dYdz = similar(mY)
-    dydzm!(dYdz, z, mY)
+    dydzm_central!(dYdz, z, mY)
 
     # update_diffusive_fluxes
     Wk_W = gas.MW * (1 ./ m_mean_MW')
     jk_star = @. - m_ρ' * Wk_W * m_Dkm * dXdz
-    jk = jk_star - mY .* sum(jk_star, dims=1)
+    jk = jk_star .- mY .* sum(jk_star, dims=1)
 
     djkdz = similar(jk)
-    dydzm!(djkdz, z, jk)
+    dydzm_central!(djkdz, z, jk)
 
-    # update_dYdx
-    # F1 = (m_ρ .* mu)' .* dYdz
-    # F2 = djkdz
-    # F3 = - gas.MW .* @view(m_wdot[1:end - 1, :])
-    # F = F1 + F2 + F3
+    F1 = mdot .* dYdz
+    F2 = djkdz
+    F3 = - gas.MW .* m_wdot
+    F = F1 + F2 + F3
+    hcat(F1[:, ind_f], F2[:, ind_f], F3[:, ind_f], F[:, ind_f])
 
-    F_Y = (m_ρ .* mu)' .* dYdz + djkdz - gas.MW .* @view(m_wdot[1:end - 1, :])
-
-    dTdz = similar(mT)
-    dydz!(dTdz, z, mT)
-
-    dTdz0 = similar(mT)
-    dydz!(dTdz0, z, dTdz .* m_λ_mix)
-
-    dTdz1 = dTdz0 - dTdz .* sum(jk .* m_cpk, dims=1)'
-    dTdz2 = dTdz1 + @view(m_wdot[end, :])
+    F_Y = mdot .* dYdz + djkdz - gas.MW .* m_wdot
 
     # update_dTdx
-    # F1 = dTdz2
-    # F2 = @. (m_ρ * mu * m_cp_mass * dTdz)
-    # F = @. dTdz2 - (m_ρ * mu * m_cp_mass * dTdz)
+    dTdz = similar(mT)
+    dydz_central!(dTdz, z, mT)
 
-    F_T = @. dTdz2 - (m_ρ * mu * m_cp_mass * dTdz)
+    dTdz0 = similar(mT)
+    dydz_central!(dTdz0, z, dTdz .* m_λ_mix)
+
+    dTdz1 = dTdz0 - dTdz .* sum(jk .* m_cpk, dims=1)'
+    dTdz2 = dTdz1 - sum(m_h_mole .* m_wdot, dims=1)'
+
+    F1 = dTdz2
+    F2 = @. (mdot * m_cp_mass * dTdz)
+    F = F1 - F2
+    hcat(dTdz1, sum(m_h_mole .* m_wdot, dims=1)', F2, F)[ind_f, :]'
+
+    F_T = @. dTdz2 - (mdot * m_cp_mass * dTdz)
 
     # boundary conditions
     F_T[1] = mT[1] - yL[end]
     F_T[end] = dTdz[end]
+    F_T[ind_f] = mT[ind_f] - T_f
 
-    F_Y[:, 1] = yL[1:end - 1] .- mY[:, 1] .- jk[:, 1] / (m_ρ[1] * mu[1])
+    F_Y[:, 1] = yL[1:end - 1] .- mY[:, 1] .- jk[:, 1] / mdot
     F_Y[:, end] = dYdz[:, end]
 
-    F_u = F_T .* 0.0
-    F_u[1] = mT[ind_f] - T_f
-
-    dudz = similar(mu)
-    dydz!(dudz, z, m_ρ .* mu)
-
-    F = vcat(F_Y, F_T', dudz')
+    F = vcat(F_Y, F_T')
 
     # Test code
-    # HR = - sum(m_h_mole .* m_wdot[1:ns, :], dims=1)'
     # @test m_cp_mass ≈ f.cp_mass rtol = 1.e-4
-    # @test m_cp_mole ≈ f.cp_mole rtol = 1.e-4
     # @test m_λ_mix ≈ f.thermal_conductivity rtol = 1.e-4
-    # @test m_wdot[1:end - 1, :] ≈ f.net_production_rates rtol = 1.e-4
+    # @test m_wdot ≈ f.net_production_rates rtol = 1.e-4
+    @test m_Dkm ≈ f.mix_diff_coeffs rtol = 1.e-3
+    # HR = - sum(m_h_mole .* m_wdot[1:ns, :], dims=1)'
     # @test HR ≈ f.heat_release_rate rtol = 1.e-4
 
     return vcat(F...)
 end
-
-cal_wdot = Wdot(f.P)
-p = zeros(nr)
-z = f.grid
-ng = length(z)
-ny = ns + 2
-yall = hcat(f.Y', f.T, f.velocity)
-yv = reshape(yall, 1, ng * ny)
-yL = vcat(f.Y[:, 1], f.T[1])
-ind_f = findfirst(f.T .> (f.T[1] + f.T[end]) / 2.0)
-T_f = f.T[ind_f]
-
-Fv = residual(gas, cal_wdot, p, z, yv, yL, ind_f, T_f)
-
-@time Fy = ForwardDiff.jacobian(yv -> residual(gas, cal_wdot, p, z, yv, yL, ind_f, T_f), yv)
-@time Fp = ForwardDiff.jacobian(p -> residual(gas, cal_wdot, p, z, yv, yL, ind_f, T_f), p)
-
-dydp = Fy \ Fp
-
-sens = dydp[ns + 2, :]
--- src/sens_flame_1d.jl
+++ src/sens_flame_1d.jl
@@ -8,8 +8,13 @@ using ForwardDiff
 using SparseArrays
 using PyCall
 using Plots
+using Test
 
-mech = "../mechanism/h2o2.yaml"
+mech = "../mechanism/1S_CH4_MP1.yaml"
+reactants = "CH4:0.5, O2:1.0, N2:3.76"
+
+# mech = "../mechanism/h2o2.yaml"
+# reactants = "H2:2.0, O2:1.0, AR:3.76"
 
 ## Call Cantera
 ct = pyimport("cantera")
@@ -18,15 +23,14 @@ ct_gas = ct.Solution(mech)
 # Simulation parameters
 p = ct.one_atm  # pressure [Pa]
 Tin = 300.0  # unburned gas temperature [K]
-reactants = "H2:2.0, O2:1.0, AR:3.76"
 ct_gas.TPX = Tin, p, reactants
 
 # Flame object
 width = 0.1  # m
 f = ct.FreeFlame(ct_gas, width=width)
-f.set_refine_criteria(ratio=3, slope=0.07, curve=0.14, prune=0.01)
+f.set_refine_criteria(ratio=3, slope=0.1, curve=0.1, prune=0.005)
 
-f.solve(loglevel=1, auto=true)
+f.solve(loglevel=8, auto=true)
 f.velocity[1]
 
 ct_sens = f.get_flame_speed_reaction_sensitivities()
@@ -38,3 +42,26 @@ nr = gas.n_reactions
 
 include("flame_1d.jl")
 
+cal_wdot = Wdot(f.P)
+p = zeros(nr)
+z = f.grid
+ng = length(z)
+ny = ns + 1
+yall = vcat(f.Y, f.T')
+yv = vcat(reshape(yall, :, 1), f.density[1] * f.velocity[1])
+yL = vcat(f.Y[:, 1], f.T[1])
+ind_f = findfirst(f.T .> 900.0)
+T_f = f.T[ind_f]
+
+Fv = residual(gas, cal_wdot, p, z, yv, yL, ind_f, T_f)
+
+@time Fy = ForwardDiff.jacobian(yv -> residual(gas, cal_wdot, p, z, yv, yL, ind_f, T_f), yv)
+@time Fp = ForwardDiff.jacobian(p -> residual(gas, cal_wdot, p, z, yv, yL, ind_f, T_f), p)
+
+dydp = Fy \ Fp
+
+sens = dydp[end, :] ./ f.velocity[1] / f.density[1]
+
+@show hcat(sens, ct_sens)
+
+cos = dot(sens ./ norm(sens), ct_sens ./ norm(ct_sens))
